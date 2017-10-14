705c0327c7bc68fe9c66382354edc6edc4fae0d9
Jack Baron <yodude107@gmail.com>
2017-10-14 03:59:21 +0100

    Set Ping Command

--- src/registry.js
+++ src/registry.js
@@ -5,10 +5,12 @@ const Backend = require('statsbot-backend')
 const Invite = require('./commands/invite')
 const Channels = require('./commands/channels')
 const Roles = require('./commands/roles')
+const SetPing = require('./commands/setPing')
 
 const Registry = new Backend.Registry()
   .addCommand(Invite)
   .addCommand(Channels)
   .addCommand(Roles)
+  .addCommand(SetPing)
 
 module.exports = Registry
