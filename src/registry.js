4979879914d2d2f39c30e756136e7782a6ecd6c6
Jack Baron <yodude107@gmail.com>
2017-10-14 03:05:04 +0100

    Channels Command

--- src/registry.js
+++ src/registry.js
@@ -3,8 +3,10 @@ const Backend = require('statsbot-backend')
 
 // Commands
 const Invite = require('./commands/invite')
+const Channels = require('./commands/channels')
 
 const Registry = new Backend.Registry()
   .addCommand(Invite)
+  .addCommand(Channels)
 
 module.exports = Registry
